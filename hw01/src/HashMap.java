79d73f221d2532e79e75df0120a321f5e41c25c8
Gavrilov <gavrilov_dan@mail.ru>
2019-01-26 19:12:20 +0300

    HashMap: added clear()? exceptions? size modifications

--- hw01/src/HashMap.java
+++ hw01/src/HashMap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class HashMap {
     private final int mod = (int)1e6;
     private List[] buckets;
@@ -17,23 +19,45 @@ public class HashMap {
     }
 
     public boolean contains(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
         return (get(key) != null);
     }
 
     public String get(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
         var hash = getIndex(key);
         return buckets[hash].getValue(key);
     }
 
     public String put(String key, String value) {
+        if (key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
         String oldValue = remove(key);
         var hash = getIndex(key);
         buckets[hash].addElement(key, value);
+        size++;
         return oldValue;
     }
 
     public String remove(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
         var hash = getIndex(key);
-        return buckets[hash].removeKey(key);
+        String tmp = buckets[hash].removeKey(key);
+        if (tmp != null) {
+            size--;
+        }
+        return tmp;
+    }
+
+    public void clear() {
+        Arrays.fill(buckets, null);
+        size = 0;
     }
 }
